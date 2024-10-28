d774609ba782588f42d7fc778ca0f85ceafe68af
mastewaldegu <mastewaldegu0@gmail.com>
2024-10-28 06:29:04 -0700

    add db configration

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const connectDB = require("./config/db.js")
 require("dotenv").config()
 const port = process.env.PORT
 
 const app = express()
+connectDB()
 app.listen(port,()=>{
     console.log(`The server is running on port number ${port}`)
 })
